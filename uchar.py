9d703180bc7fb78febf339c6a64df03901d52f56
Bunyododilov <156313999+Bunyododilov@users.noreply.github.com>
2025-07-16 18:15:31 +0500

    Update uchar.py

--- uchar.py
+++ uchar.py
@@ -68,4 +68,18 @@ def main():
 
 if __name__ == "__main__":
     main()
+# Fayl oxirida bo‘lishi kerak
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot ishlayapti!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+Thread(target=run).start()
 
